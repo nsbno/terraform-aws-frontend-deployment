f5bee60985b7d2740182a12c1f23e15644fe63ec
stekern <stekern@users.noreply.github.com>
2020-07-07 23:14:44 +0200

    Add a dependency to policy document

--- policies.tf
+++ policies.tf
@@ -28,4 +28,11 @@ data "aws_iam_policy_document" "s3_for_cross_account" {
     actions   = ["s3:Get*", "s3:Delete*", "s3:Put*"]
     resources = formatlist("arn:aws:s3:::%s/*", [for pair in local.frontend_deployment_payload.s3_source_target_pairs : pair["s3_target_bucket"]])
   }
+  /*
+  * NOTE: Without this dependency, the policy document will not be updated the first time
+  * `var.frontend_applications` has been edited (e.g., new name of a target bucket), and `apply`
+  * must be run twice. This dependency, however, forces the recreation of the policy document
+  * if the input variable has changed.
+  */
+  depends_on = [var.frontend_applications]
 }
